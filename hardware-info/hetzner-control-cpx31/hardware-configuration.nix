b1173ee1f42349cf24fbb659c16157a3b56fd7a0
Evan Hatch <evanlhatch@gmail.com>
2025-05-05 13:15:18 -0600

    init doc

---
+++ hardware-info/hetzner-control-cpx31/hardware-configuration.nix
@@ -0,0 +1,10 @@
+{ modulesPath, ... }:
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  
+  # CPX31 specs: 4 cores, 8GB RAM, 160GB disk
+}
---
+++ hardware-info/hetzner-worker-cpx21/hardware-configuration.nix
@@ -0,0 +1,10 @@
+{ modulesPath, ... }:
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  
+  # CPX21 specs: 3 cores, 4GB RAM, 80GB disk
+}
